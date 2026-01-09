fce3d46f374b1e42ba19b62ab27251dd83ecadac
Radek Chaloupka <wpeasy@gmail.com>
2026-01-09 09:12:44 +0100

    release: patch -- User (#5)
    * feature: Auth scaffolding for adminy
    
    * Update release, add changelog
    
    * Role-based policies

---
+++ app/Infrastructure/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Infrastructure\Providers;
+
+use App\Domain\FormSubmission\Models\FormSubmission;
+use App\Domain\FormSubmission\Policies\FormSubmissionPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        FormSubmission::class => FormSubmissionPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
