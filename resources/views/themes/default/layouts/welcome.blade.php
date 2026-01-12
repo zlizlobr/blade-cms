14857f4b95d304fcb0c275a21143606075de1ee8
Radek Chaloupka <wpeasy@gmail.com>
2026-01-12 10:49:28 +0100

    Create specialized welcome layout and integrate welcome page into theme system

---
+++ resources/views/themes/default/layouts/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', config('app.name', 'Laravel'))</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            @stack('inline-styles')
+        @endif
+
+        @stack('styles')
+    </head>
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        @yield('content')
+
+        @stack('scripts')
+    </body>
+</html>
