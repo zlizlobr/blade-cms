a64e589057dcddce370a2d06255daf81f5465270
Radek Chaloupka <wpeasy@gmail.com>
2026-01-12 10:52:12 +0100

    Move navigation from layouts to partials for better semantic organization

--- resources/views/themes/default/layouts/app.blade.php
+++ resources/views/themes/default/layouts/app.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('theme::layouts.navigation')
+            @include('theme::partials.navigation')
 
             <!-- Page Heading -->
             @hasSection('header')
