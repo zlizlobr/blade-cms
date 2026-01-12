7b281d786ca4b13de50516a47565b440e0585a27
Radek Chaloupka <wpeasy@gmail.com>
2026-01-12 14:14:04 +0100

    Register admin and public view namespaces in ThemeViewServiceProvider

--- app/Infrastructure/Providers/ThemeViewServiceProvider.php
+++ app/Infrastructure/Providers/ThemeViewServiceProvider.php
@@ -24,5 +24,11 @@ class ThemeViewServiceProvider extends ServiceProvider
     {
         // Register theme namespace
         View::addNamespace('theme', resource_path('views/themes/default'));
+
+        // Register admin namespace
+        View::addNamespace('admin', resource_path('views/admin'));
+
+        // Register public namespace
+        View::addNamespace('public', resource_path('views/public'));
     }
 }
